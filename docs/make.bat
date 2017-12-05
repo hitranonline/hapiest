9f9ed762020703691912ea665b01627efbc9dc91
Joshua Karns <jkarns275@gmail.com>
2017-12-04 20:27:01 -0500

    Moved docs source from src to docs/src

--- docs/make.bat
+++ docs/make.bat
@@ -7,7 +7,7 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set SOURCEDIR=source
+set SOURCEDIR=src
 set BUILDDIR=build
 set SPHINXPROJ=Hapiest
 
