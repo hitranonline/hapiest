a962689d460a253a98a25b4dda7c2910af3e33cc
Joshua Karns <jkarns275@gmail.com>
2019-06-01 20:26:41 -0400

    code quality

--- src/__main__.py
+++ src/__main__.py
@@ -1,13 +1,10 @@
 """
-This is the main module. It basically checks that there is a hapi api key, asks for one if there
-is not one, and launches the main GUI. Thats about it.
+This is the main module. It calls the startup functions, then runs the main-gui app
+run function. Thats it.
 """
 from multiprocessing import freeze_support
 import sys
 
-# This should be imported first: it verifies the correct python version is running, and moves the
-# current working directory to wherever it ought to be. Importing things for their side-effects
-# is probably bad practice, but so is using python.
 from startup import fix_cwd, check_version
 from app import run
 
