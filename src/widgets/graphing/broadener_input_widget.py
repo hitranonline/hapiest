0b736fb1dacdc64e056ed3032431491896cd686e
Joshua Karns <jkarns275@gmail.com>
2019-07-11 12:29:14 -0400

    Polishing up broadening.

--- src/widgets/graphing/broadener_input_widget.py
+++ src/widgets/graphing/broadener_input_widget.py
@@ -12,23 +12,36 @@ class BroadenerItemWidget(QWidget):
 
         self.broadener = broadener
 
-        self.selected = QCheckBox()
+        self.enabled = QCheckBox()
+        self.enabled.clicked.connect(self.__on_enabled_toggled)
+
         self.name = QLabel(broadener)
-        self.hspacer = QSpacerItem(1, 1, hPolicy = QSizePolicy.Expanding)
+        self.hspacer = QSpacerItem(1, 1, QSizePolicy.Expanding)
 
         # Spin box with two decimal places, constant size, range from 0 to 1
         self.proportion = QDoubleSpinBox()
         self.proportion.setDecimals(2)
         self.proportion.setRange(0.0, 1.0)
         self.proportion.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
+        self.proportion.setDisabled(True)
 
         self.hlayout = QHBoxLayout()
-        self.hlayout.addWidget(self.selected)
+        self.hlayout.addWidget(self.enabled)
         self.hlayout.addWidget(self.name)
         self.hlayout.addSpacerItem(self.hspacer)
         self.hlayout.addWidget(self.proportion)
 
+        self.hlayout.setContentsMargins(0, 0, 0, 0)
+
+        self.setSizePolicy(QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding))
+
         self.setLayout(self.hlayout)
+
+    def __on_enabled_toggled(self, checked):
+        self.proportion.setEnabled(checked)
+
+    def is_enabled(self):
+        return self.enabled.isChecked()
 
     def get_value(self):
         return self.proportion.value()
@@ -48,28 +61,11 @@ class BroadenerInputWidget(QWidget):
         QWidget.__init__(self, parent)
 
         self.broadeners = set()
-
-        self.selected_broadener = QComboBox()
-
-        self.add_broadener = QPushButton()
-        self.add_broadener.setText("Add Broadener")
-        self.add_broadener.pressed.connect(self.__on_add_broadener_pressed)
-
-        self.remove_checked = QPushButton()
-        self.remove_checked.setText("Remove Checked")
-        self.remove_checked.pressed.connect(self.__on_remove_checked_pressed)
-
-        self.added_broadener_names = set()
         self.broadener_items = {}
 
         self.broadener_item_layout = QVBoxLayout()
         self.broadener_items_container = QWidget()
         self.broadener_items_container.setLayout(self.broadener_item_layout)
-
-        self.controls_layout = QHBoxLayout()
-        self.controls_layout.addWidget(self.selected_broadener)
-        self.controls_layout.addWidget(self.add_broadener)
-        self.controls_layout.addWidget(self.remove_checked)
 
         self.scroll_area = QScrollArea()
         self.scroll_area.setWidgetResizable(True)
@@ -77,7 +73,8 @@ class BroadenerInputWidget(QWidget):
         self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
 
         self.main_layout = QVBoxLayout()
-        self.main_layout.addLayout(self.controls_layout)
+        self.main_layout.setSpacing(0)
+        self.main_layout.setContentsMargins(0, 0, 0, 0)
         self.main_layout.addWidget(self.scroll_area)
         self.setLayout(self.main_layout)
 
@@ -105,8 +102,9 @@ class BroadenerInputWidget(QWidget):
     def get_diluent(self):
         diluent = {}
 
-        for name in self.added_broadener_names:
-            diluent[name] = self.broadener_items[name].get_value()
+        for name in self.broadeners:
+            if self.broadener_items[name].is_enabled():
+                diluent[name] = self.broadener_items[name].get_value()
 
         return diluent
 
@@ -121,41 +119,9 @@ class BroadenerInputWidget(QWidget):
                 break
             a.widget().hide()
 
-        for name in self.added_broadener_names:
+        for name in self.broadeners:
             if name not in self.broadener_items:
                 self.broadener_items[name] = BroadenerItemWidget(name)
 
             self.broadener_items[name].show()
             self.broadener_item_layout.addWidget(self.broadener_items[name])
-
-        self.selected_broadener.clear()
-        self.selected_broadener.addItems(
-                list(self.broadeners.difference(self.added_broadener_names)))
-
-        if len(self.selected_broadener) == 0:
-            self.add_broadener.setDisabled(True)
-        else:
-            self.add_broadener.setEnabled(True)
-
-        if len(self.added_broadener_names) == 0:
-            self.remove_checked.setDisabled(True)
-        else:
-            self.remove_checked.setEnabled(True)
-
-    def as_hapi_parameter(self):
-        raise Exception("You forgot to do this")
-
-    def __on_add_broadener_pressed(self, *_args):
-        if len(self.selected_broadener) == 0:
-            return
-
-        self.added_broadener_names.add(self.selected_broadener.currentText())
-        self.update_widgets()
-
-    def __on_remove_checked_pressed(self, *_args):
-        for broadener_item in self.broadener_items.values():
-            if broadener_item.selected.isChecked() \
-                    and broadener_item.broadener in self.added_broadener_names:
-                self.added_broadener_names.remove(broadener_item.broadener)
-
-        self.update_widgets()
